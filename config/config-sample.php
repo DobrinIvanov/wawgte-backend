eee4f6d188af3b84f6487a66207150ce47bc4191
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-02 11:51:45 +0000

    add config-sample.php

---
+++ config/config-sample.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration
+define('DB_HOST', 'hostname');
+define('DB_USER', 'database_user');
+define('DB_PASS', 'database_user_password');
+define('DB_NAME', 'database_name');
+?>
