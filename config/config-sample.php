a0a3ff4e2ca19ac534e15ffe17b75b305d3cc6b8
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-04 21:04:31 +0000

    remove some files/folder

--- config/config-sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database configuration
-define('DB_HOST', 'hostname');
-define('DB_USER', 'database_user');
-define('DB_PASS', 'database_user_password');
-define('DB_NAME', 'database_name');
-?>
