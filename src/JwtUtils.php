74e95c17e5a1b0451dd8516606ae4663d8a8ed2d
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-04 22:33:24 +0000

    set up and test proof JwtUtils validateToken

--- src/JwtUtils.php
+++ src/JwtUtils.php
@@ -1,12 +1,13 @@
 <?php
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class JwtUtils {
     // Constructor method to initialize the Jwt object with secret key passed from outside
-    public function __construct(private string $secretKey){
+    public function __construct(private string $secretKey) {
     }
 
-    public function generateToken($userId) {
+    public function generateToken($userId): string{
         $payload = [
             'iss' => 'backend.wawgte.com',
             'iat' => time(),
@@ -18,10 +19,10 @@ class JwtUtils {
 
         return JWT::encode($payload, $this->secretKey, 'HS256');
     }
-    // TODO - Does not work as expected
+
     public function validateToken($token) {
         try {
-            $decoded = JWT::decode($token, new Key(self::$secretKey, 'HS256'));
+            $decoded = JWT::decode($token, new Key($this->secretKey, 'HS256'));
             return (array) $decoded->data;
         } catch (Exception $error) {
             return $error->getMessage();
