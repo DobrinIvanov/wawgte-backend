28e53edf87c53812439b06f4b4429780793a152e
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-18 15:45:43 +0000

    fix syntax error on ErrorHandler class

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -16,7 +16,7 @@ class ErrorHandler
         int $errno,
         string $errstr,
         string $errfile,
-        int$errline,
+        int $errline,
     ): bool {
         throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
     }
